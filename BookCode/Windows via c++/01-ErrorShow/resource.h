b0d932267bd2fab5f20d5805d08b34f139f3b650
sungmirr <sungmirr@gmail.com>
2012-03-24 07:30:58 +0900

    Windows via c++ : 01-ErrorShow add

---
+++ BookCode/Windows via c++/01-ErrorShow/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 01-ErrorShow.rc
+//
+#define IDI_ERRORSHOW                   101
+#define IDD_ERRORSHOW                   102
+#define IDC_ERRORCODE                   1001
+#define IDC_LOOKUP                      1002
+#define IDC_ALWAYSONTOP                 1003
+#define IDC_ERRORTEXT                   1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
